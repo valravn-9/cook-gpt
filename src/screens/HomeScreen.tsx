64f5c78021f1112787415770299507a5fc7be552
ciphr9 <dtb.brandl@live.de>
2023-04-16 21:38:10 +0200

    unused import removed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Animated } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 const HomeScreen = () => {
