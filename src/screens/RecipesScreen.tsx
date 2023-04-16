7f64f344101a1132dd5e5b2865f6afbcde9dcb8c
ciphr9 <dtb.brandl@live.de>
2023-04-16 20:26:04 +0200

    example flatlist for upcoming recipes list added

--- src/screens/RecipesScreen.tsx
+++ src/screens/RecipesScreen.tsx
@@ -1,14 +1,44 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+
+interface Recipe {
+  title: string;
+}
 
 const RecipesScreen = () => {
+  const getMockedRecipes = () => {
+    const recipes: Recipe[] = [];
+    for (let i = 0; i < 100; i++) {
+      recipes.push({ title: `Recipe ${i + 1}` });
+    }
+    return recipes;
+  };
+
+  const [recipes, setRecipes] = useState<Recipe[]>(getMockedRecipes());
   return (
     <View>
-      <Text>RecipesScreen</Text>
+      <FlatList
+        data={recipes}
+        keyExtractor={(recipe: Recipe) => recipe.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => alert(JSON.stringify(item))}>
+              <Text style={styles.recipeEntry}>{item.title}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  recipeEntry: {
+    fontSize: 18,
+    padding: 10,
+    marginBottom: 1,
+    backgroundColor: "white",
+  },
+});
 
 export default RecipesScreen;
