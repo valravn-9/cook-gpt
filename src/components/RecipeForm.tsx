d1be0dfcb5e508bc08dca09825a5bf11fafbeda3
ciphr9 <dtb.brandl@live.de>
2023-04-16 21:37:16 +0200

    initial commit of recipe form

--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -5,7 +5,7 @@ const RecipeForm = () => {
   const [title, setTitle] = useState<string>("");
 
   return (
-    <View>
+    <View style={styles.screen}>
       <Text style={styles.formLabel}>Title</Text>
       <TextInput
         value={title}
@@ -17,15 +17,20 @@ const RecipeForm = () => {
 };
 
 const styles = StyleSheet.create({
+  screen: {
+    height: "100%",
+    padding: 10,
+    backgroundColor: "white",
+  },
   formLabel: {
-    fontSize: 20,
-    marginBottom: 5,
+    fontSize: 14,
     marginLeft: 5,
   },
   textBox: {
     fontSize: 18,
     borderWidth: 1,
     borderColor: "black",
+    borderRadius: 5,
     marginBottom: 15,
     padding: 5,
     margin: 5,
