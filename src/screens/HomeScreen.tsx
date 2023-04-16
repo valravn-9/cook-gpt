bf3620cb4278c27cfa5005d89f0e188602dd8c9d
ciphr9 <dtb.brandl@live.de>
2023-04-16 20:25:49 +0200

    styling in home and profile screen adjusted

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,12 +3,22 @@ import { View, Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.homeScreen}>
+      <Text style={styles.logo}>🤖</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  homeScreen: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  logo: {
+    fontSize: 100,
+  },
+});
 
 export default HomeScreen;
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -3,12 +3,22 @@ import { View, Text, StyleSheet } from "react-native";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.profileScreen}>
+      <Text style={styles.logo}>👤</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  profileScreen: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  logo: {
+    fontSize: 100,
+  },
+});
 
 export default ProfileScreen;
