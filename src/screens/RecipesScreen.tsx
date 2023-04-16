7d216503e47bccfe05a7cb204f158af181e893ff
ciphr9 <dtb.brandl@live.de>
2023-04-16 21:37:43 +0200

    some style related changes on recipes screen

--- src/screens/RecipesScreen.tsx
+++ src/screens/RecipesScreen.tsx
@@ -1,44 +1,39 @@
-import { LinearGradient } from "expo-linear-gradient";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 
 interface Recipe {
   title: string;
 }
 
-const RecipesScreen = () => {
-  const getMockedRecipes = () => {
+const RecipesScreen = ({ navigation }: any) => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
     const recipes: Recipe[] = [];
     for (let i = 0; i < 100; i++) {
       recipes.push({ title: `Recipe ${i + 1}` });
     }
-    return recipes;
-  };
+    setRecipes(recipes);
+  }, []);
 
-  const [recipes, setRecipes] = useState<Recipe[]>(getMockedRecipes());
   return (
-    <LinearGradient colors={["#fff", "#3b5998", "#000"]} style={styles.gradient}>
-      <View style={styles.screen}>
-        <FlatList
-          data={recipes}
-          keyExtractor={(recipe: Recipe) => recipe.title}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity onPress={() => alert(JSON.stringify(item))}>
-                <Text style={styles.recipeEntry}>{item.title}</Text>
-              </TouchableOpacity>
-            );
-          }}
-        />
-      </View>
-    </LinearGradient>
+    <View style={styles.screen}>
+      <FlatList
+        data={recipes}
+        keyExtractor={(recipe: Recipe) => recipe.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => alert(JSON.stringify(item))}>
+              <Text style={styles.recipeEntry}>{item.title}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-  },
   screen: {
     flexDirection: "row",
     justifyContent: "center",
