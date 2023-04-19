cfca155b2605880c2a7f4c64bfad6fd977575381
ciphr9 <dtb.brandl@live.de>
2023-04-19 20:38:18 +0200

    switch theme test added

--- src/screens/Settings/index.test.tsx
+++ src/screens/Settings/index.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { act, fireEvent, render } from "@testing-library/react-native";
 import SettingsScreen from ".";
 
 describe("SettingsScreen", () => {
   it("should render", () => {
     render(<SettingsScreen />);
+  });
+  it("should switch theme", () => {
+    const switchTheme = jest.fn();
+    const { getByText } = render(<SettingsScreen switchTheme={switchTheme} />);
+    fireEvent.press(getByText("⚙️"));
+    expect(switchTheme).toHaveBeenCalled();
   });
 });
