6e299a59e6aa57513c92dd127f05146619f3a73a
ciphr9 <dtb.brandl@live.de>
2023-04-19 21:02:21 +0200

    camera access denied test

--- src/screens/RecipesScreen/PhotoCapture.test.tsx
+++ src/screens/RecipesScreen/PhotoCapture.test.tsx
@@ -6,4 +6,8 @@ describe("PhotoCapture", () => {
   it("should render", () => {
     render(<PhotoCapture />);
   });
+  it("should render no access to camera hint", () => {
+    const { getByText } = render(<PhotoCapture />);
+    expect(getByText("No access to camera")).toBeDefined;
+  });
 });
