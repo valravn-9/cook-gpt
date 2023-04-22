89d649982748589043ff3569ea0de9de47565c59
ciphr9 <dtb.brandl@live.de>
2023-04-22 13:11:17 +0200

    openapi request example

--- src/screens/RecipesScreen/RecipeForm.tsx
+++ src/screens/RecipesScreen/RecipeForm.tsx
@@ -3,8 +3,6 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import styles from "../../../styles";
 import { Configuration, OpenAIApi } from "openai";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 interface RecipeFormProps {
   route?: RouteProp<any>;
@@ -16,6 +14,7 @@ const RecipeForm = ({ route, navigation, theme }: RecipeFormProps) => {
   const [title, setTitle] = useState<string>("");
   const [country, setCountry] = useState<string>("Germany");
   const [recipeText, setRecipeText] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const initialRecipe = route?.params?.recipe ? route.params.recipe : { title: title };
@@ -25,20 +24,24 @@ const RecipeForm = ({ route, navigation, theme }: RecipeFormProps) => {
   const generateRecipe = async () => {
     console.log("Generating recipe");
     const requestText = `Can you generate me a recipe from ${country}`;
-    const apiKey = process.env.OPENAI_API_KEY;
-    console.log("🚀 generateRecipe  apiKey:", apiKey);
+    const apiKey = "<YOUR_API_KEY>";
 
     const configuration = new Configuration({
       apiKey: apiKey,
     });
     const openai = new OpenAIApi(configuration);
 
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: requestText }],
-    });
-    console.log("🚀  generateRecipe  completion:", completion);
-    setRecipeText(`${completion.data.choices[0].message?.content}`);
+    setLoading(true);
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: requestText }],
+      });
+      setRecipeText(`${completion.data.choices[0].message?.content}`);
+    } catch (error) {
+      setRecipeText(`Could not generate recipe.\n${error}`);
+    }
+    setLoading(false);
   };
 
   return (
@@ -59,7 +62,7 @@ const RecipeForm = ({ route, navigation, theme }: RecipeFormProps) => {
         placeholder="Enter country"
       />
       <Button title="Generate Recipe" onPress={generateRecipe} />
-      <Text>{recipeText}</Text>
+      <Text>{loading ? "Loading..." : recipeText}</Text>
     </View>
   );
 };
