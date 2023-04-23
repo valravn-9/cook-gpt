ba31bf14a339e9ab3d319846bedfd97d835325e3
ciphr9 <dtb.brandl@live.de>
2023-04-23 20:17:01 +0200

    snackbar on adding new recipe and adding new recipe ability added

--- src/screens/RecipesScreen/index.tsx
+++ src/screens/RecipesScreen/index.tsx
@@ -2,26 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Recipe } from "../../typings/recipe";
 import Screen from "../../components/Screen";
 import RecipeItem from "./RecipeItem";
+import { MD2Colors, Snackbar } from "react-native-paper";
+import { View } from "react-native";
 
 const RecipesScreen = () => {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [showSnackbar, setShowSnackbar] = useState(false);
 
-  useEffect(() => {
-    setRecipes(
-      Array<Recipe>(100)
-        .fill({ id: "1", title: "Recipe" })
-        .map((r, i) => ({ id: `${i}`, title: `${r.title} ${i + 1}` }))
-    );
-  }, []);
+  const addRecipe = () => {
+    setRecipes([
+      ...recipes,
+      { id: `${recipes.length + 1}`, title: `Recipe ${recipes.length + 1}` },
+    ]);
+    setShowSnackbar(true);
+  };
 
   return (
-    <Screen
-      TitleBarOptions={{ title: "Recipes", buttons: [{ icon: "plus", onPress: () => void 0 }] }}
-    >
-      {recipes.map((recipe: Recipe) => (
-        <RecipeItem key={recipe.id} recipe={recipe} />
-      ))}
-    </Screen>
+    <View>
+      <Screen
+        TitleBarOptions={{ title: "Recipes", buttons: [{ icon: "plus", onPress: addRecipe }] }}
+      >
+        {recipes.map((recipe: Recipe) => (
+          <RecipeItem key={recipe.id} recipe={recipe} />
+        ))}
+      </Screen>
+      <Snackbar
+        children={"Recipe added"}
+        visible={showSnackbar}
+        duration={3000}
+        onDismiss={() => setShowSnackbar(false)}
+        icon="close"
+        onIconPress={() => setShowSnackbar(false)}
+        style={{
+          backgroundColor: MD2Colors.green500,
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      />
+    </View>
   );
 };
 
