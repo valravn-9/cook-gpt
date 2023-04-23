e148b23cfcdae6bcbb2508d7e618c911f34b4cf7
ciphr9 <dtb.brandl@live.de>
2023-04-23 20:20:29 +0200

    color of titlebar buttons changed to primary theme color

--- src/components/Titlebar.tsx
+++ src/components/Titlebar.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { Appbar } from "react-native-paper";
+import { Appbar, useTheme } from "react-native-paper";
 import { IButtonProps, ITitleBarProps } from "../typings/titleBar";
 
 const TitleBar = ({ title, buttons, backButton }: ITitleBarProps) => {
+  const { colors } = useTheme();
   return (
     <Appbar.Header>
-      {backButton ? <Appbar.BackAction onPress={backButton.onPress} /> : null}
+      {backButton ? (
+        <Appbar.BackAction onPress={backButton.onPress} color={colors.primary} />
+      ) : null}
       <Appbar.Content title={title} />
       {buttons?.map((button: IButtonProps) => {
-        return <Appbar.Action key={button.icon} icon={button.icon} onPress={button.onPress} />;
+        return (
+          <Appbar.Action
+            key={button.icon}
+            icon={button.icon}
+            onPress={button.onPress}
+            color={colors.primary}
+          />
+        );
       })}
     </Appbar.Header>
   );
