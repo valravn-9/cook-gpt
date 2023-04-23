b727bc53c7edd8254a1daa849983a19783206d0e
ciphr9 <dtb.brandl@live.de>
2023-04-23 19:09:39 +0200

    weird star removed

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { CommonActions, NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
