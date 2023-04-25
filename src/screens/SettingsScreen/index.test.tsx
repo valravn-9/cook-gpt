4b9600d69e65a82d1335a7bf78d9aa0a90af76c1
ciphr9 <dtb.brandl@live.de>
2023-04-25 19:53:23 +0200

    settingsscreen test file fixed

--- src/screens/SettingsScreen/index.test.tsx
+++ src/screens/SettingsScreen/index.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import SettingsScreen from ".";
+import LightTheme from "../../themes/light";
 
 describe("SettingsScreen", () => {
   it("should render", () => {
-    render(<SettingsScreen />);
+    render(<SettingsScreen theme={LightTheme} switchTheme={jest.fn()} />);
   });
 });
