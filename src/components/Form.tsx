7a86e4f586f8d05a02d64f9ebaa1aa8754d85a04
ciphr9 <dtb.brandl@live.de>
2023-04-27 21:38:54 +0200

    onSave optional in Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ interface IProps {
   title: string;
   visible: boolean;
   onCancel: () => void;
-  onSave: (item: any) => void;
+  onSave?: (item: any) => void;
   item: any;
   children: ReactNode;
 }
@@ -15,20 +15,12 @@ const Form = ({ title, visible, onCancel, onSave, children, item }: IProps) => {
   const { colors } = useTheme();
   return (
     <Portal>
-      <Modal
-        visible={visible}
-        onDismiss={onCancel}
-        contentContainerStyle={{ position: "absolute", left: 0, right: 0, bottom: 0, top: 0 }}
-      >
+      <Modal visible={visible} onDismiss={onCancel} contentContainerStyle={{ position: "absolute", left: 0, right: 0, bottom: 0, top: 0 }}>
         <Surface style={{ height: "100%" }}>
           <Appbar.Header statusBarHeight={0}>
             <Appbar.Action icon="close" onPress={onCancel} color={colors.primary} />
             <Appbar.Content title={title} />
-            <Appbar.Action
-              icon="content-save"
-              onPress={() => onSave(item)}
-              color={colors.primary}
-            />
+            {onSave ? <Appbar.Action icon="content-save" onPress={() => onSave(item)} color={colors.primary} /> : void 0}
           </Appbar.Header>
           <ScrollView style={{ padding: 10 }}>{children}</ScrollView>
         </Surface>
