61b04abf48d7188af47840d7cde0ec9f91d98a13
ciphr9 <dtb.brandl@live.de>
2023-04-29 17:33:54 +0200

    visibility logic changed in details view

--- src/screens/RecipesScreen/RecipeDetails.tsx
+++ src/screens/RecipesScreen/RecipeDetails.tsx
@@ -6,13 +6,12 @@ import { Recipe } from "../../typings/recipe";
 
 interface IProps {
   recipe?: Recipe;
-  visible: boolean;
   onClose: () => void;
 }
 
-const RecipeDetails = ({ recipe, visible, onClose }: IProps) => {
+const RecipeDetails = ({ recipe, onClose }: IProps) => {
   return (
-    <Form title={recipe?.title ? recipe.title : "Recipe Details"} visible={visible} onCancel={onClose} item={recipe}>
+    <Form title={recipe?.title ? recipe.title : "Recipe Details"} onCancel={onClose} item={recipe}>
       <View style={{ gap: 10 }}>
         {recipe?.country ? <Text>From {recipe.country}</Text> : void 0}
         {recipe?.persons ? <Text>{recipe.persons} persons</Text> : void 0}
