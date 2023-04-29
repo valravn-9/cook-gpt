62026051e9335f558308eab319bd67c8b5e8d9fc
ciphr9 <dtb.brandl@live.de>
2023-04-30 01:42:21 +0200

    icons and veggie indicators added

--- src/screens/RecipesScreen/RecipeItem.tsx
+++ src/screens/RecipesScreen/RecipeItem.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 
 import { List, Card, Divider, IconButton, useTheme } from "react-native-paper";
-import { Recipe } from "../../typings/recipe";
+import { Recipe, Veggie } from "../../typings/recipe";
 import { View } from "react-native";
 
 interface IProps {
   recipe: Recipe;
   onPress: () => void;
   onDelete: () => void;
+  onEdit: () => void;
 }
 
-const RecipeItem = ({ recipe, onPress, onDelete }: IProps) => {
+const RecipeItem = ({ recipe, onPress, onEdit, onDelete }: IProps) => {
   const { colors } = useTheme();
   return (
     <Card.Content>
       <View style={{ flexDirection: "row" }}>
-        <List.Item style={{ flex: 1 }} title={recipe.title} onPress={onPress} left={(props) => <List.Icon {...props} icon={"file-document"} />} />
+        <List.Item
+          style={{ flex: 1 }}
+          title={recipe.title}
+          onPress={onPress}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon={recipe.veggie === Veggie.VEGAN ? "leaf-circle-outline" : recipe.veggie === Veggie.VEGETARIAN ? "leaf-circle" : "food-turkey"}
+            />
+          )}
+        />
+        <IconButton icon={"pencil"} onPress={onEdit} iconColor={colors.primary} />
         <IconButton icon={"delete"} onPress={onDelete} iconColor={colors.primary} />
       </View>
       <Divider />
